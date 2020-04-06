66f4fd11a37714051ab7ea9f827fc57580c6d9f1
rootcanopy <dave.trees201@gmail.com>
2020-04-06 22:21:34 +0200

    .py is running in the browser, next move is to connect to database and start populating data

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import env
+from flask import Flask, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_DB"] = "awesome-reads"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def base():
+    return "Hello World"
+
+if __name__ == '__main__':
+            app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=True)
--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["MONGO_URI"] = "mongodb+srv://rootcanopy:Di1LJQyxmTBKIvCj@mycluster-t6yqh.mongodb.net/?retryWrites=true&w=majority"
+os.environ["MONGO_URI"] = "mongodb+srv://rootcanopy:Di1LJQyxmTBKIvCj@mycluster-t6yqh.mongodb.net/awesome-reads?retryWrites=true&w=majority"
 
-os.eviron["PORT"] = "5000"
+os.environ["PORT"] = "5000"
