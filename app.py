0c878cad4d8ffa0bca0982dc9c481d8da2648066
rootcanopy <dave.trees201@gmail.com>
2020-04-07 01:56:20 +0200

    added requirements file and setting up heroku

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+#mongo = PyMongo(app)
+
 app.config["MONGO_DB"] = "awesome-reads"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-mongo = PyMongo(app)
 
 @app.route("/")
 def base():
