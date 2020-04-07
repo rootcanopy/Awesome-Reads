bdfd01d53a1f3c991bd0110f9f57ae6eb8c4a65b
rootcanopy <dave.trees201@gmail.com>
2020-04-07 02:51:15 +0200

    mongo is connected and running.

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import os
 import env
-from flask import Flask, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-#mongo = PyMongo(app)
-
-app.config["MONGO_DB"] = "awesome-reads"
+#app.config["MONGO_DB"] = "awesome-reads"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
+mongo = PyMongo(app)
 
-@app.route("/")
-def base():
-    return "Hello World"
+@app.route('/')
+@app.route('/main_page') # chnaged for setting up
+def main():
+    return render_template("base.html",
+                            users=mongo.db.users.find())
 
 if __name__ == '__main__':
             app.run(host=os.environ.get('IP'),
