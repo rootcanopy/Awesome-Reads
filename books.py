213cea6343e08d871b3ae2d36f22e284e5f47449
rootcanopy <dave.trees201@gmail.com>
2020-04-07 18:40:44 +0200

    slow day, have just started wiring up the reg form

--- books.py
+++ books.py
@@ -4,24 +4,31 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 
 #app.config["MONGO_DB"] = "awesome-reads"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
+
 mongo = PyMongo(app)
 
+# MAIN PAGE
 @app.route('/')
-@app.route('/main_page') # chnaged for setting up
-def main_page():
-    return render_template("index.html")
+@app.route('/home') # chnaged for setting up
+def home():
+    return render_template('home.html')
                             #users=mongo.db.users.find())
 
 @app.route('/user')
 #@login_required
 def user():
-    return render_template("user.html",
-                            user = mongo.db.user.find())
+    return render_template('user.html', username=['username'],
+                            user = mongo.db.users.find())
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
     
 
 if __name__ == '__main__':
