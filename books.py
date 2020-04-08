211b8a701dd987176b236d7b91d298bffae29d8a
rootcanopy <dave.trees201@gmail.com>
2020-04-08 05:17:46 +0200

    quite a few bugs to get through, i think its from the way ive down the registration

--- books.py
+++ books.py
@@ -1,8 +1,7 @@
 import os
 import env
 from config import Config
-from flask import Flash
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from forms import RegistrationForm, LoginForm
@@ -34,22 +33,25 @@ def user(username):
     return render_template('user.html', user=user, title='Profile')
                             
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('user.html', title='Register', form=form)
+    #return render_template('user.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        #TODO
-    return render_template('login.html', title='Login', form=form)
-    
+        flash(f'Welcome Back {form.username.data}, theres loads of new books to enjoy!', 'success')
+        return redirect(url_for('home')) 
+    else:
+        flash('Login Failed. Check yo\'self', 'danger')
+        return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
             app.run(host=os.environ.get('IP'),
